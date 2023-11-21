1eab91dcfe21ed3e783934810bbccd5eb02adcc0
aleson pro <rolfaleson.pro@gmail.com>
2023-11-21 23:01:27 +0800

    feat added homepage

--- YeahBuddy-Altifit/app/src/main/java/com/example/altifit/Home_Activity.kt
+++ YeahBuddy-Altifit/app/src/main/java/com/example/altifit/Home_Activity.kt
@@ -1,11 +1,19 @@
 package com.example.altifit
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class Home_Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        fun openNewActivity(view: View) {
+            val intent = Intent(this, Mobility_Activity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ YeahBuddy-Altifit/app/src/main/java/com/example/altifit/Mobility_Activity.kt
@@ -0,0 +1,11 @@
+package com.example.altifit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Mobility_Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mobility)
+    }
+}
