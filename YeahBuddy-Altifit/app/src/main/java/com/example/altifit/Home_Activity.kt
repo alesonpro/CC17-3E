7d79d253360c13e73f338a0b49abcfe28dd5b814
aleson pro <rolfaleson.pro@gmail.com>
2023-12-05 23:13:50 +0800

    feat added new workout categories screen

--- YeahBuddy-Altifit/app/src/main/java/com/example/altifit/Home_Activity.kt
+++ YeahBuddy-Altifit/app/src/main/java/com/example/altifit/Home_Activity.kt
@@ -24,6 +24,30 @@ class Home_Activity : AppCompatActivity() {
             val intent = Intent(this, Deadlift_Activity::class.java)
             startActivity(intent)
         }
+
+        val agilityImageView: ImageView = findViewById(R.id.agility)
+        agilityImageView.setOnClickListener {
+            val intent = Intent(this, Agility_Activity::class.java)
+            startActivity(intent)
+        }
+
+        val curlImageView: ImageView = findViewById(R.id.curl)
+        curlImageView.setOnClickListener {
+            val intent = Intent(this, Curl_Activity::class.java)
+            startActivity(intent)
+        }
+
+        val calImageView: ImageView = findViewById(R.id.calisthenic)
+        calImageView.setOnClickListener {
+            val intent = Intent(this, Calisthenics_Activity::class.java)
+            startActivity(intent)
+        }
+
+        val benchImageView: ImageView = findViewById(R.id.bench)
+        benchImageView.setOnClickListener {
+            val intent = Intent(this, Setup_Activity::class.java)
+            startActivity(intent)
+        }
     }
 }
 
