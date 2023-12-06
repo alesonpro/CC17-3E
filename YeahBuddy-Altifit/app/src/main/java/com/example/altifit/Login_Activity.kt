20932a71811c71fb0439ff4d2e2e7b5ecba4e78c
aleson pro <rolfaleson.pro@gmail.com>
2023-12-06 13:29:12 +0800

    feat added login function

--- YeahBuddy-Altifit/app/src/main/java/com/example/altifit/Login_Activity.kt
+++ YeahBuddy-Altifit/app/src/main/java/com/example/altifit/Login_Activity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class Login_Activity : AppCompatActivity() {
 
@@ -21,6 +23,29 @@ class Login_Activity : AppCompatActivity() {
         register_button.setOnClickListener{
             val Intent = Intent(this,Register_Activity::class.java)
             startActivity(Intent);
+        }
+
+
+        val usernameEditText: EditText = findViewById(R.id.editTextUsername)
+        val passwordEditText: EditText = findViewById(R.id.editTextPassword)
+        val loginButton: Button = findViewById(R.id.buttonLogin)
+
+        loginButton.setOnClickListener {
+            val username = usernameEditText.text.toString()
+            val password = passwordEditText.text.toString()
+
+            if (username == "admin" && password == "admin") {
+                // If admin credentials match, grant access (navigate to admin screen or perform desired action)
+                // For example:
+                // val intent = Intent(this, AdminActivity::class.java)
+                // startActivity(intent)
+
+                // For demonstration, show a success message
+                Toast.makeText(this, "Login Successful as Admin", Toast.LENGTH_SHORT).show()
+            } else {
+                // If the credentials are incorrect, show an error message
+                Toast.makeText(this, "Invalid credentials", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
